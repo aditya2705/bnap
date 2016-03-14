409f13a5a76150f7a910d9022ddfc66bbdb3e53c
swainroshankumar <reo.swain@gmail.com>
2016-03-14 16:15:34 +0530

    custom event object pushed

---
+++ app/src/main/java/com/alphalabz/businessapp/Objects/CustomEventCardObject.java
@@ -0,0 +1,19 @@
+package com.alphalabz.businessapp.Objects;
+
+/**
+ * Created by swainroshankumar on 10/03/16.
+ */
+public class CustomEventCardObject {
+    public String titleText,dateText,timeText;
+    public int image;
+
+
+    public CustomEventCardObject(String titleText, String dateText, String timeText, int image) {
+        this.titleText = titleText;
+        this.dateText = dateText;
+        this.timeText = timeText;
+        this.image = image;
+    }
+}
+
+
